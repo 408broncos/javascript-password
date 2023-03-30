8a4c668dea18a73404b8d6c1c8038171f6d25b03
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-03-29 17:47:33 -0700

    add character, number and alphabet options for variables.

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,15 @@ var enter;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//adding the special character options
+character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "{", "[", "}", "]", "|", ";", ":", "'", "<", ">", ".", "?", "/", "`", "~", "+", "=", "}", "]",];
+
+//adding number options
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//adding alphabet options
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k" ,"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
